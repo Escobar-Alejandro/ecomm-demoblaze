9d0cbb7643e9127cb65f44491244c6d01f897812
Escobar-Alejandro <escobar.alejandro@outlook.com>
2026-02-27 00:29:35 -0300

    Updated pageManager.fixture

--- fixtures/pageManager.fixture.ts
+++ fixtures/pageManager.fixture.ts
@@ -1,13 +1,25 @@
 import { test as base } from '@playwright/test';
-import { ProductListPage } from '@poms';
+import { ProductListPage, CartPage, ProductDetailsPage, Header } from '@poms';
 
 interface PageFixtures {
     productListPage: ProductListPage;
+    cartPage: CartPage;
+    productDetailsPage: ProductDetailsPage;
+    header: Header;
 }
 
 const base2 = base.extend<PageFixtures>({
     productListPage: async ({ page }, use) => {
         await use(new ProductListPage(page));
+    },
+    cartPage: async ({ page }, use) => {
+        await use(new CartPage(page));
+    },
+    productDetailsPage: async ({ page }, use) => {
+        await use(new ProductDetailsPage(page));
+    },
+    header: async ({ page }, use) => {
+        await use(new Header(page));
     }
 });
 
