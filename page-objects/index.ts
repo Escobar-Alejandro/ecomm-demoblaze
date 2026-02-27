889c54f1b40bc327554d063cc76b2c5c238e7da7
Escobar-Alejandro <escobar.alejandro@outlook.com>
2026-02-27 00:29:04 -0300

    Updated page-object index

--- page-objects/index.ts
+++ page-objects/index.ts
@@ -1,2 +1,5 @@
 export { default as BasePage } from './BasePage';
 export { default as ProductListPage } from './ProductListPage';
+export { default as ProductDetailsPage } from './ProductDetailsPage';
+export { default as CartPage } from './CartPage';
+export { default as Header } from './Header';
